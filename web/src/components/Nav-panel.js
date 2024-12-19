ab93c5098baa283a025ba7832ff8007a2b9bb8ff
P0rtOs <145544969+P0rtOs@users.noreply.github.com>
2024-12-19 17:51:50 +0200

    Changed sidebar style and font

--- web/src/components/Nav-panel.js
+++ web/src/components/Nav-panel.js
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router";
-import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
+import {
+    FaChevronRight,
+    FaChevronLeft,
+    FaUser,
+    FaUserTie,
+    FaHome,
+} from "react-icons/fa";
 
 const NavigationPanel = ({ onToggle }) => {
     const [isOpen, setIsOpen] = useState(true); // Состояние панели
@@ -44,15 +50,22 @@ const NavigationPanel = ({ onToggle }) => {
 
     // Навигационные ссылки
     const navLinks = [
-        { to: "/admin", label: "Admin Page", roles: ["admin"] },
+        {
+            to: "/admin",
+            label: "Admin Page",
+            icon: <FaUserTie />,
+            roles: ["admin"],
+        },
         {
             to: "/accountant",
             label: "Accountant Page",
+            icon: <FaHome />,
             roles: ["admin", "accountant"],
         },
         {
             to: "/user",
             label: "User Page",
+            icon: <FaUser />,
             roles: ["admin", "accountant", "user"],
         },
     ];
@@ -85,18 +98,19 @@ const NavigationPanel = ({ onToggle }) => {
             <div className="flex flex-col items-center mt-3 w-full">
                 {navLinks
                     .filter((link) => link.roles.includes(role)) // Фильтруем ссылки по роли
-                    .map(({ to, label }) => (
+                    .map(({ to, label, icon }) => (
                         <NavLink
                             key={to}
                             to={to}
                             className={({ isActive }) =>
-                                `flex items-center justify-center w-full h-12 px-3 mt-2 rounded ${
+                                `flex items-center w-full h-12 px-3 mt-2 rounded ${
                                     isActive
                                         ? "bg-blue-500 text-white"
                                         : "hover:bg-gray-700 hover:text-gray-300"
                                 }`
                             }
                         >
+                            {icon}
                             {isOpen && (
                                 <span className="ml-2 text-sm font-medium">
                                     {label}
@@ -104,29 +118,6 @@ const NavigationPanel = ({ onToggle }) => {
                             )}
                         </NavLink>
                     ))}
-            </div>
-
-            {/* Нижняя часть панели */}
-            <div className="mt-auto mb-3">
-                <NavLink
-                    to="/profile"
-                    className="flex items-center justify-center w-12 h-12 rounded-full hover:bg-gray-700 hover:text-gray-300"
-                >
-                    <svg
-                        className="w-6 h-6"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-                        />
-                    </svg>
-                </NavLink>
             </div>
         </div>
     );
